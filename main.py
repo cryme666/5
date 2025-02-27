31af171a6994db592472970742c7c7eb38482a6c
cryme666 <125838508+cryme666@users.noreply.github.com>
2025-02-27 19:02:17 +0200

    upd

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
 import pygame
-import sys
 pygame.init()
-WIDTH,HEIGHT = 800,600
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Ping Pong")
+background_color = (0, 0, 0)
+paddle_width = 20
+paddle_height = 100
+paddle_color = (255, 255, 255)
 
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('PING PONG')
+paddle1_x = 30
+paddle1_y = (height - paddle_height) // 2
 
+paddle2_x = width - 50
+paddle2_y = (height - paddle_height) // 2
 
-while True:
-    pygame.time.Clock().tick(60)
+ball_radius = 10
+ball_color = (255, 255, 255)
+ball_x = width // 2
+ball_y = height // 2
+
+paddle_speed = 7
+ball_speed_x, ball_speed_y = 5,5
+
+paddle1 = pygame.Rect(paddle1_x,paddle1_y,paddle_width,paddle_height)
+paddle2 = pygame.Rect(paddle2_x,paddle2_y,paddle_width,paddle_height)
+
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
 
-    screen.fill((255,255,255))
-    pygame.draw.circle(screen,(0,0,0),(0,0),100,5)
 
+    key_pressed = pygame.key.get_pressed()
+
+    if key_pressed[pygame.K_w] and paddle1.top > 0:
+        paddle1.move_ip(0,-paddle_speed)
+
+    if key_pressed[pygame.K_s] and paddle1.bottom < height:
+        paddle1.move_ip(0,paddle_speed)
+
+    #! прописати рух другого гравця
+
+    
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
+    
+
+    if ball_y - ball_radius <= 0 or ball_y + ball_radius >= height:
+        ball_speed_y *= -1
+
+    
+    if ball_x - ball_radius <= 0 or ball_x + ball_radius >= width:
+        ball_speed_x *= -1
+
+    screen.fill(background_color)
+
+    if paddle1.collidepoint(ball_x - ball_radius,ball_y) or paddle1.collidepoint(ball_x + ball_radius,ball_y):
+        ball_speed_x *= -1
+
+    pygame.draw.rect(screen,paddle_color,paddle1)
+    pygame.draw.rect(screen,paddle_color,paddle2)
+    pygame.draw.circle(screen, ball_color, (ball_x, ball_y), ball_radius)
+    
     pygame.display.flip()
 
+    pygame.time.Clock().tick(60)
+pygame.quit()
 
